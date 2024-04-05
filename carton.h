692deb84bceb06051e6b67d2d0020fc661b8852a
Rodrigo <rfnobando@gmail.com>
2024-04-05 12:40:00 -0300

    Primer commit

---
+++ carton.h
@@ -0,0 +1,7 @@
+#ifndef CARTON_H_INCLUDED
+#define CARTON_H_INCLUDED
+
+struct Carton;
+typedef struct Carton * CartonPtr;
+
+#endif // CARTON_H_INCLUDED
---
+++ jugador.h
@@ -0,0 +1,7 @@
+#ifndef JUGADOR_H_INCLUDED
+#define JUGADOR_H_INCLUDED
+
+struct Jugador;
+typedef struct Jugador * JugadorPtr;
+
+#endif // JUGADOR_H_INCLUDED
